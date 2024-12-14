b80fafca0eaad97bcfebf6c67f1e0fc35f585139
Home <iraperec00@mail.ru>
2024-12-14 04:28:40 +0300

    n

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat.equals(cat1));
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    public Cat() {
+    }
+
+    private String catName;
+
+    public String getCatName() {
+        return catName;
+    }
+
+    public void setCatName(String catName) {
+        this.catName = catName;
+    }
+}
+
